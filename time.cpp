20de73715404a2aa02ef5bb487fa86692bbf3fcd
Cù Nguyễn Huy Thái Tuấn <charlotteglorixia2005@gmail.com>
2024-05-13 19:04:25 +0700

    Update time.cpp

--- time.cpp
+++ time.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Time {
@@ -10,7 +11,13 @@ class Time {
         Time();
         Time(int, int, int);
         void SetTime(int, int, int);
-        void Increment();
+        Time operator++();
+        Time operator++(int);
+        Time operator--();
+        Time operator--(int);
+        bool operator>(Time const&) const;
+        bool operator<(Time const&) const;
+        int compare(Time const&) const;
         void DisplayTime();
 };
 
@@ -21,12 +28,8 @@ Time::Time()
     secs = 0;
 }
 
-Time::Time(int h, int m, int s)
-{
-    this->hrs = h;
-    this->mins = m;
-    this->secs = s;
-}
+Time::Time(int h, int m, int s) : hrs(h), mins(m), secs(s)
+{}
 
 void Time::SetTime(int h, int m, int s)
 {
@@ -35,37 +38,97 @@ void Time::SetTime(int h, int m, int s)
     secs = s;
 }
 
-void Time::Increment()
-{
-    secs++;
-    if (secs >= 60)
-    {
-        secs = 0;
-        mins++;
-        if (mins >= 60)
-        {
-            mins = 0;
-            hrs++;
-            if (hrs >= 24)
-            {
-                hrs = 0;
-            }
-        }
+Time Time::operator++() {
+    ++secs;
+    if (secs >= 60){
+        ++mins;
+        secs -= 60;
     }
+    if (mins >= 60){
+        ++hrs;
+        mins -= 60;
+    }
+    if (hrs >= 24) {
+        hrs -= 24;
+    }
+    return *this;
+}
+
+Time Time::operator++(int) {
+    Time tmp(*this);
+    ++(*this);
+    return tmp;
+}
+
+Time Time::operator--() {
+    --secs;
+    if (secs == -1) {
+        --mins;
+        secs += 60;
+    }
+    if (mins == -1) {
+        --hrs;
+        mins += 60;
+    }
+    if (hrs == -1) {
+        hrs += 24;
+    }
+    return *this;
+}
+
+Time Time::operator--(int) {
+    Time tmp(*this);
+    --(*this);
+    return tmp;
+}
+
+int Time::compare(Time const& other) const {
+    int result = hrs - other.hrs;
+    if (result == 0) {
+        result = mins - other.mins;
+        if (result == 0)
+            result = secs - other.secs;
+    }
+    return result;
+}
+
+bool Time::operator>(Time const& other) const {
+    return compare(other) > 0;
+}
+
+bool Time::operator<(Time const& other) const {
+    return compare(other) < 0;
 }
 
 void Time::DisplayTime()
 {
-    cout << hrs << ":" << mins << ":" << secs << endl;
+    if (hrs < 10 && mins < 10 && secs < 10)
+        cout << "0" + to_string(hrs) << ":" << "0" + to_string(mins) << ":" << "0" + to_string(secs) << endl;
+    else if (hrs < 10 && mins < 10)
+        cout << "0" + to_string(hrs) << ":" << "0" + to_string(mins) << ":" << to_string(secs) << endl;
+    else if (hrs < 10 && secs < 10)
+        cout << "0" + to_string(hrs) << ":" << to_string(mins) << ":" << "0" + to_string(secs) << endl;
+    else if (mins < 10 && secs < 10)
+        cout << to_string(hrs) << ":" << "0" + to_string(mins) << ":" << "0" + to_string(secs) << endl;
+    else if (hrs < 10)
+        cout << "0" + to_string(hrs) << ":" << to_string(mins) << ":" << to_string(secs) << endl;
+    else if (mins < 10)
+        cout << to_string(hrs) << ":" << "0" + to_string(mins) << ":" << to_string(secs) << endl;
+    else if (secs < 10)
+        cout << to_string(hrs) << ":" << to_string(mins) << ":" << "0" + to_string(secs) << endl;
+    else
+        cout << to_string(hrs) << ":" << to_string(mins) << ":" << to_string(secs) << endl;
 }
 
 int main() {
     Time t1;
     t1.DisplayTime();
-    t1.SetTime(23, 59, 59);
+    t1.SetTime(12, 34, 59);
     t1.DisplayTime();
-    t1.Increment();
+    t1--;
     t1.DisplayTime();
     return 0;
 }
+
+
 
